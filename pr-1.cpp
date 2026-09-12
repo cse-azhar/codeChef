88381faf00e89fb1d88bfe9fdfb2937ebe2d9e1b
cse-azhar <cse.azhar@gmail.com>
2026-09-13 00:44:24 +0600

    added a new cpp file

---
+++ pr-1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr1[n], arr2[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr1[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            arr2[i] = arr1[i];
+        }
+
+        
+    }
+}
